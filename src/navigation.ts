256bc05aa118d4dec70cebac71f98da5ff90d27e
Garrett M <garrett@rair.tech>
2025-03-25 10:34:23 -0600

    fix: resolve header rendering issues and add proper navigation configuration

---
+++ src/navigation.ts
@@ -0,0 +1,61 @@
+export const headerData = {
+  links: [
+    {
+      text: 'Home',
+      href: '/',
+    },
+    {
+      text: 'About',
+      href: '/about',
+    },
+    {
+      text: 'Features',
+      href: '/features',
+    },
+    {
+      text: 'Documentation',
+      href: '/docs',
+    },
+    {
+      text: 'Blog',
+      href: '/blog',
+    },
+  ],
+};
+
+export const footerData = {
+  links: [
+    {
+      text: 'Home',
+      href: '/',
+    },
+    {
+      text: 'About',
+      href: '/about',
+    },
+    {
+      text: 'Features',
+      href: '/features',
+    },
+    {
+      text: 'Documentation',
+      href: '/docs',
+    },
+    {
+      text: 'Blog',
+      href: '/blog',
+    },
+  ],
+  socialLinks: [
+    {
+      text: 'Twitter',
+      href: 'https://twitter.com/rairtech',
+      icon: 'tabler:brand-twitter',
+    },
+    {
+      text: 'GitHub',
+      href: 'https://github.com/RAIReth',
+      icon: 'tabler:brand-github',
+    },
+  ],
+}; 
