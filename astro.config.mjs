0d77f684970314de6fea215f2d183ffd6666726a
Garrett M <garrett@rair.tech>
2025-03-26 21:37:21 -0600

    fix: update icon configuration to properly handle local icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,8 +49,7 @@ export default defineConfig({
           'ethereum',
           'ethereum-color',
         ],
-      },
-      exclude: ['local'],
+      }
     }),
 
     ...whenExternalScripts(() =>
